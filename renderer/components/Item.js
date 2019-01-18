225853e7e0098033a14491dec56de61ead7919a1
Aaron Holland <aholland@alumi-guard.com>
2019-01-18 08:43:53 -0500

    Changed ItemList to table

--- renderer/components/Item.js
+++ renderer/components/Item.js
@@ -1,5 +1,10 @@
-import { Pane, Button, Text } from "evergreen-ui";
-import Link from "next/link";
-
+import { Table } from "evergreen-ui";
 export default props => (
+  <Table.Row>
+    <Table.TextCell>{props.type.toUpperCase()}</Table.TextCell>
+    <Table.TextCell>{props.grade.toUpperCase()}</Table.TextCell>
+    <Table.TextCell>{props.width}</Table.TextCell>
+    <Table.TextCell>{props.finialCount}</Table.TextCell>
+    <Table.TextCell>{props.qty}</Table.TextCell>
+  </Table.Row>
 );
--- renderer/components/ItemList.js
+++ renderer/components/ItemList.js
@@ -1,10 +1,28 @@
-import React, { Component } from "react";
-import { Pane } from "evergreen-ui";
+import { Table } from "evergreen-ui";
 import Item from "./Item";
 
-export default class extends Component {
-  render() {
-    return (
-    );
-  }
-}
+export default props => (
+  <Table.Body padding={50}>
+    <Table.Head>
+      <Table.TextCell textAlign="center">Items</Table.TextCell>
+    </Table.Head>
+    <Table.Head>
+      <Table.TextCell>Item</Table.TextCell>
+      <Table.TextCell>Grade</Table.TextCell>
+      <Table.TextCell>Width/Opening ( Inches )</Table.TextCell>
+      <Table.TextCell>Finial Count</Table.TextCell>
+      <Table.TextCell>Item Count</Table.TextCell>
+    </Table.Head>
+    <Table.Body>
+      {props.items.map(item => (
+        <Item
+          type={item.type}
+          grade={item.grade}
+          width={item.width}
+          finialCount={item.finialCount}
+          qty={item.qty}
+        />
+      ))}
+    </Table.Body>
+  </Table.Body>
+);
