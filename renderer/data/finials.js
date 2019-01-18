b2a548a73f391debf3217454ddf9343e7dd33d72
Aaron Holland <aholland@alumi-guard.com>
2019-01-18 08:44:21 -0500

    Added Finial Data

---
+++ renderer/data/finials.js
@@ -0,0 +1,891 @@
+export default [
+  {
+    number: 73022363,
+    description: "FINIAL-5/8X1 GDN QUAD ALUM",
+    grade: "GUARDIAN",
+    type: "QUAD",
+    color: "MILL"
+  },
+  {
+    number: 73023669,
+    description: 'FINIAL-5/8X1" GDN QUAD ALUM BG',
+    grade: "GUARDIAN",
+    type: "QUAD",
+    color: "BEIGE"
+  },
+  {
+    number: 73023664,
+    description: 'FINIAL-5/8X1" GDN QUAD ALUM BK',
+    grade: "GUARDIAN",
+    type: "QUAD",
+    color: "BLACK"
+  },
+  {
+    number: 73023665,
+    description: 'FINIAL-5/8X1" GDN QUAD ALUM BZ',
+    grade: "GUARDIAN",
+    type: "QUAD",
+    color: "BRONZE"
+  },
+  {
+    number: 73023667,
+    description: 'FINIAL-5/8X1" GDN QUAD ALUM GR',
+    grade: "GUARDIAN",
+    type: "QUAD",
+    color: "GREEN"
+  },
+  {
+    number: 73023668,
+    description: 'FINIAL-5/8X1" GDN QUAD ALUM GY',
+    grade: "GUARDIAN",
+    type: "QUAD",
+    color: "GREY"
+  },
+  {
+    number: 73023666,
+    description: 'FINIAL-5/8X1" GDN QUAD ALUM LB',
+    grade: "GUARDIAN",
+    type: "QUAD",
+    color: "LIGHT BRONZE"
+  },
+  {
+    number: 73023670,
+    description: 'FINIAL-5/8X1" GDN QUAD ALUM WH',
+    grade: "GUARDIAN",
+    type: "QUAD",
+    color: "WHITE"
+  },
+  {
+    number: 0,
+    description: 'FINIAL-5/8X1" GDN QUAD ALUM GD',
+    grade: "GUARDIAN",
+    type: "QUAD",
+    color: "GOLD"
+  },
+  {
+    number: 73022367,
+    description: "FINIAL-5/8X1 GDN TRIAD ALUM",
+    grade: "GUARDIAN",
+    type: "TRIAD",
+    color: "MILL"
+  },
+  {
+    number: 73023693,
+    description: 'FINIAL-5/8X1" GDN TRIAD ALUM BG',
+    grade: "GUARDIAN",
+    type: "TRIAD",
+    color: "BEIGE"
+  },
+  {
+    number: 73023692,
+    description: 'FINIAL-5/8X1" GDN TRIAD ALUM BK',
+    grade: "GUARDIAN",
+    type: "TRIAD",
+    color: "BLACK"
+  },
+  {
+    number: 73023698,
+    description: 'FINIAL-5/8X1" GDN TRIAD ALUM BZ',
+    grade: "GUARDIAN",
+    type: "TRIAD",
+    color: "BRONZE"
+  },
+  {
+    number: 73023695,
+    description: 'FINIAL-5/8X1" GDN TRIAD ALUM GR',
+    grade: "GUARDIAN",
+    type: "TRIAD",
+    color: "GREEN"
+  },
+  {
+    number: 73023696,
+    description: 'FINIAL-5/8X1" GDN TRIAD ALUM GY',
+    grade: "GUARDIAN",
+    type: "TRIAD",
+    color: "GREY"
+  },
+  {
+    number: 73023697,
+    description: 'FINIAL-5/8X1" GDN TRIAD ALUM LB',
+    grade: "GUARDIAN",
+    type: "TRIAD",
+    color: "LIGHT BRONZE"
+  },
+  {
+    number: 73023694,
+    description: 'FINIAL-5/8X1" GDN TRIAD ALUM WH',
+    grade: "GUARDIAN",
+    type: "TRIAD",
+    color: "WHITE"
+  },
+  {
+    number: 0,
+    description: 'FINIAL-5/8X1" GDN TRIAD ALUM GD',
+    grade: "GUARDIAN",
+    type: "TRIAD",
+    color: "GOLD"
+  },
+  {
+    number: 73022604,
+    description: "PLUG-PICKET-GDN BK",
+    grade: "GUARDIAN",
+    type: "PLUG",
+    color: "BLACK"
+  },
+  {
+    number: 73022605,
+    description: "PLUG-PICKET-GDN WH",
+    grade: "GUARDIAN",
+    type: "PLUG",
+    color: "WHITE"
+  },
+  {
+    number: 34110647,
+    description: "FINIAL-5/8 RES TRIAD ALUM MILL",
+    grade: "RESIDENTIAL",
+    type: "TRIAD",
+    color: "MILL"
+  },
+  {
+    number: 73010673,
+    description: "FINIAL-5/8 RES TRIAD ALUM BK",
+    grade: "RESIDENTIAL",
+    type: "TRIAD",
+    color: "BLACK"
+  },
+  {
+    number: 73020400,
+    description: "FINIAL-5/8 RES TRIAD ALUM MB",
+    grade: "RESIDENTIAL",
+    type: "TRIAD",
+    color: "MATTE"
+  },
+  {
+    number: 73010680,
+    description: "FINIAL-5/8 RES TRIAD ALUM PW",
+    grade: "RESIDENTIAL",
+    type: "TRIAD",
+    color: "PWTR"
+  },
+  {
+    number: 73010694,
+    description: "FINIAL-5/8 RES TRIAD ALUM WH",
+    grade: "RESIDENTIAL",
+    type: "TRIAD",
+    color: "WHITE"
+  },
+  {
+    number: 73012678,
+    description: "FINIAL-5/8 RES TRIAD ALUM GD",
+    grade: "RESIDENTIAL",
+    type: "TRIAD",
+    color: "GOLD"
+  },
+  {
+    number: 73040925,
+    description: "FINIAL-5/8 RES TRIAD ALUM BG",
+    grade: "RESIDENTIAL",
+    type: "TRIAD",
+    color: "BEIGE"
+  },
+  {
+    number: 73040926,
+    description: "FINIAL-5/8 RES TRIAD ALUM BZ",
+    grade: "RESIDENTIAL",
+    type: "TRIAD",
+    color: "FL BRONZE"
+  },
+  {
+    number: 73040927,
+    description: "FINIAL-5/8 RES TRIAD ALUM GR",
+    grade: "RESIDENTIAL",
+    type: "TRIAD",
+    color: "GREEN "
+  },
+  {
+    number: 73040928,
+    description: "FINIAL-5/8 RES TRIAD ALUM GY",
+    grade: "RESIDENTIAL",
+    type: "TRIAD",
+    color: "GRAY"
+  },
+  {
+    number: 73040929,
+    description: "FINIAL-5/8 RES TRIAD ALUM LB",
+    grade: "RESIDENTIAL",
+    type: "TRIAD",
+    color: "LT BRONZE"
+  },
+  {
+    number: 34110646,
+    description: "FINIAL-5/8 RES QUAD ALUM MILL",
+    grade: "RESIDENTIAL",
+    type: "QUAD",
+    color: "MILL"
+  },
+  {
+    number: 73010674,
+    description: "FINIAL-5/8 RES QUAD ALUM BK",
+    grade: "RESIDENTIAL",
+    type: "QUAD",
+    color: "BLACK"
+  },
+  {
+    number: 73020401,
+    description: "FINIAL-5/8 RES QUAD ALUM MB",
+    grade: "RESIDENTIAL",
+    type: "QUAD",
+    color: "MATTE"
+  },
+  {
+    number: 73010681,
+    description: "FINIAL-5/8 RES QUAD ALUM PW",
+    grade: "RESIDENTIAL",
+    type: "QUAD",
+    color: "PWTR"
+  },
+  {
+    number: 73010695,
+    description: "FINIAL-5/8 RES QUAD ALUM WH",
+    grade: "RESIDENTIAL",
+    type: "QUAD",
+    color: "WHITE"
+  },
+  {
+    number: 73012676,
+    description: "FINIAL-5/8 RES QUAD ALUM GD",
+    grade: "RESIDENTIAL",
+    type: "QUAD",
+    color: "GOLD"
+  },
+  {
+    number: 73040920,
+    description: "FINIAL-5/8 RES QUAD ALUM BG",
+    grade: "RESIDENTIAL",
+    type: "QUAD",
+    color: "BEIGE"
+  },
+  {
+    number: 73040921,
+    description: "FINIAL-5/8 RES QUAD ALUM BZ",
+    grade: "RESIDENTIAL",
+    type: "QUAD",
+    color: "FL BRONZE"
+  },
+  {
+    number: 73040922,
+    description: "FINIAL-5/8 RES QUAD ALUM GR",
+    grade: "RESIDENTIAL",
+    type: "QUAD",
+    color: "GREEN "
+  },
+  {
+    number: 73040923,
+    description: "FINIAL-5/8 RES QUAD ALUM GY",
+    grade: "RESIDENTIAL",
+    type: "QUAD",
+    color: "GRAY"
+  },
+  {
+    number: 73040924,
+    description: "FINIAL-5/8 RES QUAD ALUM LB",
+    grade: "RESIDENTIAL",
+    type: "QUAD",
+    color: "LT BRONZE"
+  },
+  {
+    number: 34110648,
+    description: "FINIAL-5/8 RES FLEUR DE LIS ALUM MILL",
+    grade: "RESIDENTIAL",
+    type: "FLEUR DE LIS",
+    color: "MILL"
+  },
+  {
+    number: 73040931,
+    description: "FINIAL-5/8 RES FLEUR DE LIS ALUM BK",
+    grade: "RESIDENTIAL",
+    type: "FLEUR DE LIS",
+    color: "BLACK"
+  },
+  {
+    number: 73040650,
+    description: "FINIAL-5/8 RES FLEUR DE LIS ALUM MB",
+    grade: "RESIDENTIAL",
+    type: "FLEUR DE LIS",
+    color: "MATTE"
+  },
+  {
+    number: 73040651,
+    description: "FINIAL-5/8 RES FLEUR DE LIS ALUM PW",
+    grade: "RESIDENTIAL",
+    type: "FLEUR DE LIS",
+    color: "PWTR"
+  },
+  {
+    number: 73040936,
+    description: "FINIAL-5/8 RES FLEUR DE LIS ALUM WH",
+    grade: "RESIDENTIAL",
+    type: "FLEUR DE LIS",
+    color: "WHITE"
+  },
+  {
+    number: 73040649,
+    description: "FINIAL-5/8 RES FLEUR DE LIS ALUM GD",
+    grade: "RESIDENTIAL",
+    type: "FLEUR DE LIS",
+    color: "GOLD"
+  },
+  {
+    number: 73040930,
+    description: "FINIAL-5/8 RES FLEUR DE LIS ALUM BG",
+    grade: "RESIDENTIAL",
+    type: "FLEUR DE LIS",
+    color: "BEIGE"
+  },
+  {
+    number: 73040932,
+    description: "FINIAL-5/8 RES FLEUR DE LIS ALUM BZ",
+    grade: "RESIDENTIAL",
+    type: "FLEUR DE LIS",
+    color: "FL BRONZE"
+  },
+  {
+    number: 73040933,
+    description: "FINIAL-5/8 RES FLEUR DE LIS ALUM GR",
+    grade: "RESIDENTIAL",
+    type: "FLEUR DE LIS",
+    color: "GREEN "
+  },
+  {
+    number: 73040934,
+    description: "FINIAL-5/8 RES FLEUR DE LIS ALUM GY",
+    grade: "RESIDENTIAL",
+    type: "FLEUR DE LIS",
+    color: "GRAY"
+  },
+  {
+    number: 73040935,
+    description: "FINIAL-5/8 RES FLEUR DE LIS ALUM LB",
+    grade: "RESIDENTIAL",
+    type: "FLEUR DE LIS",
+    color: "LT BRONZE"
+  },
+  {
+    number: 34110650,
+    description: "FINIAL-3/4 CML TRIAD ALUM MF",
+    grade: "COMMERCIAL",
+    type: "TRIAD",
+    color: "MILL "
+  },
+  {
+    number: 73010708,
+    description: "FINIAL-3/4 CML TRIAD ALUM BK",
+    grade: "COMMERCIAL",
+    type: "TRIAD",
+    color: "BLACK"
+  },
+  {
+    number: 73020519,
+    description: "FINIAL-3/4 CML TRIAD ALUM MB",
+    grade: "COMMERCIAL",
+    type: "TRIAD",
+    color: "MATTE"
+  },
+  {
+    number: 73010714,
+    description: "FINIAL-3/4 CML TRIAD ALUM PW",
+    grade: "COMMERCIAL",
+    type: "TRIAD",
+    color: "PWTR"
+  },
+  {
+    number: 73010721,
+    description: "FINIAL-3/4 CML TRIAD ALUM WH",
+    grade: "COMMERCIAL",
+    type: "TRIAD",
+    color: "WHITE"
+  },
+  {
+    number: 73012679,
+    description: "FINIAL-3/4 CML TRIAD ALUM GD",
+    grade: "COMMERCIAL",
+    type: "TRIAD",
+    color: "GOLD"
+  },
+  {
+    number: 73040942,
+    description: "FINIAL-3/4 CML TRIAD ALUM BG",
+    grade: "COMMERCIAL",
+    type: "TRIAD",
+    color: "BEIGE"
+  },
+  {
+    number: 73040943,
+    description: "FINIAL-3/4 CML TRIAD ALUM BZ",
+    grade: "COMMERCIAL",
+    type: "TRIAD",
+    color: "FL BRONZE"
+  },
+  {
+    number: 73040944,
+    description: "FINIAL-3/4 CML TRIAD ALUM GR",
+    grade: "COMMERCIAL",
+    type: "TRIAD",
+    color: "GREEN "
+  },
+  {
+    number: 73040945,
+    description: "FINIAL-3/4 CML TRIAD ALUM GY",
+    grade: "COMMERCIAL",
+    type: "TRIAD",
+    color: "GRAY"
+  },
+  {
+    number: 73040946,
+    description: "FINIAL-3/4 CML TRIAD ALUM LB",
+    grade: "COMMERCIAL",
+    type: "TRIAD",
+    color: "LT BRONZE"
+  },
+  {
+    number: 34110649,
+    description: "FINIAL-3/4 CML QUAD ALUM MILL",
+    grade: "COMMERCIAL",
+    type: "QUAD",
+    color: "MILL"
+  },
+  {
+    number: 73010709,
+    description: "FINIAL-3/4 CML QUAD ALUM BK",
+    grade: "COMMERCIAL",
+    type: "QUAD",
+    color: "BLACK"
+  },
+  {
+    number: 73020520,
+    description: "FINIAL-3/4 CML QUAD ALUM MB",
+    grade: "COMMERCIAL",
+    type: "QUAD",
+    color: "MATTE"
+  },
+  {
+    number: 73010715,
+    description: "FINIAL-3/4 CML QUAD ALUM PW",
+    grade: "COMMERCIAL",
+    type: "QUAD",
+    color: "PWTR"
+  },
+  {
+    number: 73010722,
+    description: "FINIAL-3/4 CML QUAD ALUM WH",
+    grade: "COMMERCIAL",
+    type: "QUAD",
+    color: "WHITE"
+  },
+  {
+    number: 73012677,
+    description: "FINIAL-3/4 CML QUAD ALUM GD",
+    grade: "COMMERCIAL",
+    type: "QUAD",
+    color: "GOLD"
+  },
+  {
+    number: 73040937,
+    description: "FINIAL-3/4 CML QUAD ALUM BG",
+    grade: "COMMERCIAL",
+    type: "QUAD",
+    color: "BEIGE"
+  },
+  {
+    number: 73040938,
+    description: "FINIAL-3/4 CML QUAD ALUM BZ",
+    grade: "COMMERCIAL",
+    type: "QUAD",
+    color: "FL BRONZE"
+  },
+  {
+    number: 73040939,
+    description: "FINIAL-3/4 CML QUAD ALUM GR",
+    grade: "COMMERCIAL",
+    type: "QUAD",
+    color: "GREEN "
+  },
+  {
+    number: 73040940,
+    description: "FINIAL-3/4 CML QUAD ALUM GY",
+    grade: "COMMERCIAL",
+    type: "QUAD",
+    color: "GRAY"
+  },
+  {
+    number: 73040941,
+    description: "FINIAL-3/4 CML QUAD ALUM LB",
+    grade: "COMMERCIAL",
+    type: "QUAD",
+    color: "LT BRONZE"
+  },
+  {
+    number: 34110651,
+    description: "FINIAL-3/4 CML FLEUR DE LIS ALUM MF",
+    grade: "COMMERCIAL",
+    type: "FLEUR DE LIS",
+    color: "MILL "
+  },
+  {
+    number: 73040948,
+    description: "FINIAL-3/4 CML FLEUR DE LIS ALUM BK",
+    grade: "COMMERCIAL",
+    type: "FLEUR DE LIS",
+    color: "BLACK"
+  },
+  {
+    number: 73040653,
+    description: "FINIAL-3/4 CML FLEUR DE LIS ALUM MB",
+    grade: "COMMERCIAL",
+    type: "FLEUR DE LIS",
+    color: "MATTE"
+  },
+  {
+    number: 73040648,
+    description: "FINIAL-3/4 CML FLEUR DE LIS ALUM PW",
+    grade: "COMMERCIAL",
+    type: "FLEUR DE LIS",
+    color: "PWTR"
+  },
+  {
+    number: 73040953,
+    description: "FINIAL-3/4 CML FLEUR DE LIS ALUM WH",
+    grade: "COMMERCIAL",
+    type: "FLEUR DE LIS",
+    color: "WHITE"
+  },
+  {
+    number: 73040518,
+    description: "FINIAL-3/4 CML FLEUR DE LIS ALUM GD",
+    grade: "COMMERCIAL",
+    type: "FLEUR DE LIS",
+    color: "GOLD"
+  },
+  {
+    number: 73040947,
+    description: "FINIAL-3/4 CML FLEUR DE LIS ALUM BG",
+    grade: "COMMERCIAL",
+    type: "FLEUR DE LIS",
+    color: "BEIGE"
+  },
+  {
+    number: 73040949,
+    description: "FINIAL-3/4 CML FLEUR DE LIS ALUM BZ",
+    grade: "COMMERCIAL",
+    type: "FLEUR DE LIS",
+    color: "FL BRONZE"
+  },
+  {
+    number: 73040950,
+    description: "FINIAL-3/4 CML FLEUR DE LIS ALUM GR",
+    grade: "COMMERCIAL",
+    type: "FLEUR DE LIS",
+    color: "GREEN "
+  },
+  {
+    number: 73040951,
+    description: "FINIAL-3/4 CML FLEUR DE LIS ALUM GY",
+    grade: "COMMERCIAL",
+    type: "FLEUR DE LIS",
+    color: "GRAY"
+  },
+  {
+    number: 73040952,
+    description: "FINIAL-3/4 CML FLEUR DE LIS ALUM LB",
+    grade: "COMMERCIAL",
+    type: "FLEUR DE LIS",
+    color: "LT BRONZE"
+  },
+  {
+    number: 34110653,
+    description: "FINIAL-1 IND TRIAD ALUM MF",
+    grade: "INDUSTRIAL",
+    type: "TRIAD",
+    color: "MILL"
+  },
+  {
+    number: 73040962,
+    description: "FINIAL-1 IND TRIAD ALUM BK",
+    grade: "INDUSTRIAL",
+    type: "TRIAD",
+    color: "BLACK"
+  },
+  {
+    number: 73040563,
+    description: "FINIAL-1 IND TRIAD ALUM MB",
+    grade: "INDUSTRIAL",
+    type: "TRIAD",
+    color: "MATTE"
+  },
+  {
+    number: 73040564,
+    description: "FINIAL-1 IND TRIAD ALUM PW",
+    grade: "INDUSTRIAL",
+    type: "TRIAD",
+    color: "PWTR"
+  },
+  {
+    number: 73040967,
+    description: "FINIAL-1 IND TRIAD ALUM WH",
+    grade: "INDUSTRIAL",
+    type: "TRIAD",
+    color: "WHITE"
+  },
+  {
+    number: 73040562,
+    description: "FINIAL-1 IND TRIAD ALUM GD",
+    grade: "INDUSTRIAL",
+    type: "TRIAD",
+    color: "GOLD"
+  },
+  {
+    number: 73040961,
+    description: "FINIAL-1 IND TRIAD ALUM BG",
+    grade: "INDUSTRIAL",
+    type: "TRIAD",
+    color: "BEIGE"
+  },
+  {
+    number: 73040963,
+    description: "FINIAL-1 IND TRIAD ALUM BZ",
+    grade: "INDUSTRIAL",
+    type: "TRIAD",
+    color: "FL BRONZE"
+  },
+  {
+    number: 73040964,
+    description: "FINIAL-1 IND TRIAD ALUM GR",
+    grade: "INDUSTRIAL",
+    type: "TRIAD",
+    color: "GREEN "
+  },
+  {
+    number: 73040965,
+    description: "FINIAL-1 IND TRIAD ALUM GY",
+    grade: "INDUSTRIAL",
+    type: "TRIAD",
+    color: "GRAY"
+  },
+  {
+    number: 73040966,
+    description: "FINIAL-1 IND TRIAD ALUM LB",
+    grade: "INDUSTRIAL",
+    type: "TRIAD",
+    color: "LT BRONZE"
+  },
+  {
+    number: 34110652,
+    description: "FINIAL-1 IND QUAD ALUM MF",
+    grade: "INDUSTRIAL",
+    type: "QUAD",
+    color: "MILL"
+  },
+  {
+    number: 73040955,
+    description: "FINIAL-1 IND QUAD ALUM BK",
+    grade: "INDUSTRIAL",
+    type: "QUAD",
+    color: "BLACK"
+  },
+  {
+    number: 73040555,
+    description: "FINIAL-1 IND QUAD ALUM MB",
+    grade: "INDUSTRIAL",
+    type: "QUAD",
+    color: "MATTE"
+  },
+  {
+    number: 73040556,
+    description: "FINIAL-1 IND QUAD ALUM PW",
+    grade: "INDUSTRIAL",
+    type: "QUAD",
+    color: "PWTR"
+  },
+  {
+    number: 73040960,
+    description: "FINIAL-1 IND QUAD ALUM WH",
+    grade: "INDUSTRIAL",
+    type: "QUAD",
+    color: "WHITE"
+  },
+  {
+    number: 73040404,
+    description: "FINIAL-1 IND QUAD ALUM GD",
+    grade: "INDUSTRIAL",
+    type: "QUAD",
+    color: "GOLD"
+  },
+  {
+    number: 73040954,
+    description: "FINIAL-1 IND QUAD ALUM BG",
+    grade: "INDUSTRIAL",
+    type: "QUAD",
+    color: "BEIGE"
+  },
+  {
+    number: 73040956,
+    description: "FINIAL-1 IND QUAD ALUM BZ",
+    grade: "INDUSTRIAL",
+    type: "QUAD",
+    color: "FL BRONZE"
+  },
+  {
+    number: 73040957,
+    description: "FINIAL-1 IND QUAD ALUM GR",
+    grade: "INDUSTRIAL",
+    type: "QUAD",
+    color: "GREEN "
+  },
+  {
+    number: 73040958,
+    description: "FINIAL-1 IND QUAD ALUM GY",
+    grade: "INDUSTRIAL",
+    type: "QUAD",
+    color: "GRAY"
+  },
+  {
+    number: 73040959,
+    description: "FINIAL-1 IND QUAD ALUM LB",
+    grade: "INDUSTRIAL",
+    type: "QUAD",
+    color: "LT BRONZE"
+  },
+  {
+    number: 34110654,
+    description: "FINIAL-1 IND FLEUR DE LIS ALUM MF",
+    grade: "INDUSTRIAL",
+    type: "FLEUR DE LIS",
+    color: "MILL"
+  },
+  {
+    number: 73040969,
+    description: "FINIAL-1 IND FLEUR DE LIS ALUM BK",
+    grade: "INDUSTRIAL",
+    type: "FLEUR DE LIS",
+    color: "BLACK"
+  },
+  {
+    number: 73040398,
+    description: "FINIAL-1 IND FLEUR DE LIS ALUM MB",
+    grade: "INDUSTRIAL",
+    type: "FLEUR DE LIS",
+    color: "MATTE"
+  },
+  {
+    number: 73040401,
+    description: "FINIAL-1 IND FLEUR DE LIS ALUM PW",
+    grade: "INDUSTRIAL",
+    type: "FLEUR DE LIS",
+    color: "PWTR"
+  },
+  {
+    number: 73040974,
+    description: "FINIAL-1 IND FLEUR DE LIS ALUM WH",
+    grade: "INDUSTRIAL",
+    type: "FLEUR DE LIS",
+    color: "WHITE"
+  },
+  {
+    number: 73040381,
+    description: "FINIAL-1 IND FLEUR DE LIS ALUM GD",
+    grade: "INDUSTRIAL",
+    type: "FLEUR DE LIS",
+    color: "GOLD"
+  },
+  {
+    number: 73040968,
+    description: "FINIAL-1 IND FLEUR DE LIS ALUM BG",
+    grade: "INDUSTRIAL",
+    type: "FLEUR DE LIS",
+    color: "BEIGE"
+  },
+  {
+    number: 73040970,
+    description: "FINIAL-1 IND FLEUR DE LIS ALUM BZ",
+    grade: "INDUSTRIAL",
+    type: "FLEUR DE LIS",
+    color: "FL BRONZE"
+  },
+  {
+    number: 73040971,
+    description: "FINIAL-1 IND FLEUR DE LIS ALUM GR",
+    grade: "INDUSTRIAL",
+    type: "FLEUR DE LIS",
+    color: "GREEN "
+  },
+  {
+    number: 73040972,
+    description: "FINIAL-1 IND FLEUR DE LIS ALUM GY",
+    grade: "INDUSTRIAL",
+    type: "FLEUR DE LIS",
+    color: "GRAY"
+  },
+  {
+    number: 73040973,
+    description: "FINIAL-1 IND FLEUR DE LIS ALUM LB",
+    grade: "INDUSTRIAL",
+    type: "FLEUR DE LIS",
+    color: "LT BRONZE"
+  },
+  {
+    number: 34109185,
+    description: "PLUG-PICKET-5/8 BK",
+    grade: "RESIDENTIAL",
+    type: "PLUG",
+    color: "BLACK"
+  },
+  {
+    number: 34109186,
+    description: "PLUG-PICKET-5/8 PW",
+    grade: "RESIDENTIAL",
+    type: "PLUG",
+    color: "PEWTER"
+  },
+  {
+    number: 34109187,
+    description: "PLUG-PICKET-5/8 WH",
+    grade: "RESIDENTIAL",
+    type: "PLUG",
+    color: "WHITE"
+  },
+  {
+    number: 34109158,
+    description: "PLUG-PICKET-3/4 BK",
+    grade: "COMMERCIAL",
+    type: "PLUG",
+    color: "BLACK"
+  },
+  {
+    number: 34109159,
+    description: "PLUG-PICKET-3/4 PW",
+    grade: "COMMERCIAL",
+    type: "PLUG",
+    color: "PEWTER"
+  },
+  {
+    number: 34109160,
+    description: "PLUG-PICKET-3/4 WH",
+    grade: "COMMERCIAL",
+    type: "PLUG",
+    color: "WHITE"
+  },
+  {
+    number: 73040975,
+    description: "PLUG-PICKET-1 IND BK",
+    grade: "INDUSTRIAL",
+    type: "PLUG",
+    color: "BLACK"
+  },
+  {
+    number: 73040976,
+    description: "PLUG-PICKET-1 IND WH",
+    grade: "INDUSTRIAL",
+    type: "PLUG",
+    color: "WHITE"
+  }
+];
